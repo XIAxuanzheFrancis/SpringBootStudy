aa6d432955aed806f9113d3242a923f975dd993a
xiaxuanzhe <3058634081@qq.com>
2023-11-17 18:12:21 +0100

    helloworld and springboot banner

---
+++ src/test/java/com/xuanzhe/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuanzhe.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
